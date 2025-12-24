6b402da4bc256b091c9cee9fc4f2f3b66960dd96
Baonguyen1711 <Baonguyen17112004@gmail.com>
2025-12-24 21:51:01 +0700

    test ci/cd lần 7

--- auth-service/handlers/authHandlers.ts
+++ auth-service/handlers/authHandlers.ts
@@ -51,6 +51,7 @@ export const signUp = async (
     console.log('test ci/cd lần 4:');
     console.log('test ci/cd lần 5:');
     console.log('test ci/cd lần 6:');
+    console.log('test ci/cd lần 7:');
     if (!awsUserId) {
       throw new Error('Failed to get Cognito UserSub');
     }
