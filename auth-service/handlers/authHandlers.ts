6c7241f9b366d275511c88d5b024bd8c3564727f
Baonguyen1711 <Baonguyen17112004@gmail.com>
2025-12-25 11:43:53 +0700

    test ci/cd ver 2

--- auth-service/handlers/authHandlers.ts
+++ auth-service/handlers/authHandlers.ts
@@ -45,15 +45,7 @@ export const signUp = async (
     const awsUserId = await signUpService(registerUser);
 
     console.log('Cognito UserSub:', awsUserId);
-    console.log('test ci/cd:');
-    console.log('test ci/cd lần 2:');
-    console.log('test ci/cd lần 3:');
-    console.log('test ci/cd lần 4:');
-    console.log('test ci/cd lần 5:');
-    console.log('test ci/cd lần 6:');
-    console.log('test ci/cd lần 7:');
-    console.log('test ci/cd lần 8:');
-    console.log('test ci/cd lần 9:');
+    console.log('test ci/cd ver 2:');
     if (!awsUserId) {
       throw new Error('Failed to get Cognito UserSub');
     }
